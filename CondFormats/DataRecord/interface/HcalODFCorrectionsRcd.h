0466e792c55a013c1e2bd32115a44801aee6ae58
Audrius <audrius.mecionis@gmail.com>
2014-08-29 11:56:25 +0200

    added EventSetup record

---
+++ CondFormats/DataRecord/interface/HcalODFCorrectionsRcd.h
@@ -0,0 +1,25 @@
+#ifndef HcalODFCorrectionsRcd_HcalODFCorrectionsRcd_h
+#define HcalODFCorrectionsRcd_HcalODFCorrectionsRcd_h
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     HcalODFCorrectionsRcd
+// 
+/**\class HcalODFCorrectionsRcd HcalODFCorrectionsRcd.h CondFormats/DataRecord/interface/HcalODFCorrectionsRcd.h
+
+ Description: [one line class summary]
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      Audrius Mecionis
+// Created:     Fri, 29 Aug 2014 09:54:07 GMT
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class HcalODFCorrectionsRcd : public edm::eventsetup::EventSetupRecordImplementation<HcalODFCorrectionsRcd> {};
+
+#endif
---
+++ CondFormats/DataRecord/src/HcalODFCorrectionsRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     HcalODFCorrectionsRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      Audrius Mecionis
+// Created:     Fri, 29 Aug 2014 09:54:07 GMT
+
+#include "CondFormats/DataRecord/interface/HcalODFCorrectionsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(HcalODFCorrectionsRcd);
